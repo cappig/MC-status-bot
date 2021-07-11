e185ceab009345ecc256df379721d192785178b8
cappig <prvi.sedmi@gmail.com>
2021-07-11 10:30:40 +0000

    Spelling changes
    * Fix some spelling mistakes in help.js

--- commands/help.js
+++ commands/help.js
@@ -5,15 +5,14 @@ module.exports = {
 
     execute(message) { 
 
-        const description = 'This bot is still in early development. Please report any bugs on the [Github](https://github.com/cappig/MC-status-bot/issues)\n\n' +
-                            '`mc!log` - turn logging on or of\n' +
+        const description = 'This bot is still in early development. Please report any bugs on [Github](https://github.com/cappig/MC-status-bot/issues)\n\n' +
                             '`mc!ping` - ping a minecraft server\n' +
-                            '`mc!setip` - set a ip that the bot will monitor\n' +
-                            '`mc!log` - turn logging on or of\n' +
+                            '`mc!setip` - set an ip that the bot will monitor\n' +
+                            '`mc!log` - turn logging on or off\n' +
                             '`mc!setup` - set up the channels that will display the status of a selected server\n' +
-                            '`mc!rmchann` - removes the monitoring channels\n' +
-                            '`mc!news` - get the latest articles from minecraft.net\n' +
-                            '\n[Add to server](https://discord.com/api/oauth2/authorize?client_id=816747912888975362&permissions=8&scope=bot) | [Privacy policy](https://github.com/cappig/MC-status-bot/blob/main/miscellaneous/Privacy_policy.md) | [Github](https://github.com/cappig/MC-status-bot)'
+                            '`mc!rmchann` - remove the monitoring channels\n' +
+                            '`mc!news` - see the latest articles from minecraft.net\n' +
+                            '\n[Invite to a server](https://discord.com/api/oauth2/authorize?client_id=816747912888975362&permissions=8&scope=bot) | [Privacy policy](https://github.com/cappig/MC-status-bot/blob/main/miscellaneous/Privacy_policy.md) | [Github](https://github.com/cappig/MC-status-bot)'
 
         const embed = new Discord.MessageEmbed()
             .setColor('#008000')
