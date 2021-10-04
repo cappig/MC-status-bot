93991c0e757ba7fe1f1840dc43b713f6bf9a0de2
cappig <prvi.sedmi@gmail.com>
2021-10-04 21:56:01 +0000

    Make pinger use cron
    * Pinger will now be run on every 5th minute in a hour insted of every
      five minutes after the app starts.
    * Npm update

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,10 @@
 const pinger = require('../modules/pinger.js')
-const guildscan = require('../modules/guildscan.js')
+var cron = require('node-cron');
+const guildscan = require('../modules/guildscan.js');
 
 module.exports = {
     name: 'ready',
-    async execute(client) {
+    execute(client) {
         console.log('\x1b[32m\x1b[1m%s\x1b[0m', 'The bot is up and running!');
 
         // Update activity every hour so that it doesn't expire
@@ -14,13 +15,15 @@ module.exports = {
 
         // Scan for guilds not in the db, the ones that were added when the bot was offline
         if (process.argv.slice(2) == "--guildscan") {
-            console.log('Started guild scan.');
-            await guildscan.execute(client);
+            (async () => {
+                console.log('Started guild scan.');
+                await guildscan.execute(client);
+            })();
         }
 
         // Call the pinger every 5 minutes
-        setInterval(() => {
-            pinger.execute(client)
-        }, 300000);
+        cron.schedule('*/5 * * * *', () => {
+            pinger.execute(client);
+        });          
     }
 }
